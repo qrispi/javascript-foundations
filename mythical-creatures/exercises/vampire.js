c82ff5f4faaeb584eb67224354f307c59a98237d
Christopher Baum <christopherabaum@gmail.com>
2022-12-05 22:32:06 -0600

    Update drink method

--- mythical-creatures/exercises/vampire.js
+++ mythical-creatures/exercises/vampire.js
@@ -7,9 +7,11 @@ class Vampire {
             this.pet = pet
         }
         this.thirsty = true
+        this.ouncesDrank = 0
     }
     drink() {
         this.thirsty = false
+        this.ouncesDrank += 10
     }
 }
 
