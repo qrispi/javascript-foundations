af114c510677fbbde6b68bf9f80f43e3fb33112a
Christopher Baum <christopherabaum@gmail.com>
2022-12-05 23:32:23 -0600

    Refactor vmapire with ternary operators

--- mythical-creatures/exercises/vampire.js
+++ mythical-creatures/exercises/vampire.js
@@ -1,21 +1,13 @@
 class Vampire {
     constructor(name, pet) {
         this.name = name
-        if (pet === undefined) {
-            this.pet = 'bat'
-        } else {
-            this.pet = pet
-        }
+        pet === undefined ? this.pet = 'bat' : this.pet = pet
         this.thirsty = true
         this.ouncesDrank = 0
     }
     drink() {
         this.thirsty = false
-        if (this.ouncesDrank < 50) {
-            this.ouncesDrank += 10
-        } else {
-            return `I\'m too full to drink anymore!`
-        }
+        return this.ouncesDrank < 50 ? this.ouncesDrank += 10 : `I\'m too full to drink anymore!`
     }
 }
 
