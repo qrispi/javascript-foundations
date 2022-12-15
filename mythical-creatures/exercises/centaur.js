62a850d649dae086ac704098446238421a5fb7c8
Christopher Baum <christopherabaum@gmail.com>
2022-12-14 22:17:04 -0600

    Complete Centaur and update README

---
+++ mythical-creatures/exercises/centaur.js
@@ -0,0 +1,65 @@
+class Centaur {
+    constructor(creature) {
+        this.name = creature.name
+        this.breed = creature.type
+        this.cranky = false
+        this.standing = true
+        this.layingDown = false
+        this.activities = 0
+    }
+
+    shootBow() {
+        if (this.activities === 2) {
+            this.cranky = true
+            this.activities++
+            return 'Twang!!!'
+        } else if (this.cranky || this.layingDown) {
+            return 'NO!'
+        } else {
+            this.activities++
+            return 'Twang!!!'
+        }
+    }
+
+    run() {
+        if (this.activities === 2) {
+            this.cranky = true
+            this.activities++
+            return 'Clop clop clop clop!!!'
+        } else if (this.cranky || this.layingDown) {
+            return 'NO!'
+        } else {
+            this.activities++
+            return 'Clop clop clop clop!!!'
+        }
+    }
+
+    sleep() {
+        if (this.standing) {
+            return 'NO!'
+        } else {
+            this.cranky = false
+            return 'ZZZZ'
+        }
+    }
+
+    layDown() {
+        this.standing = !this.standing
+        this.layingDown = !this.layingDown
+    }
+
+    standUp() {
+       this.layDown()
+    }
+
+    drinkPotion() {
+        if (this.standing) {
+            this.cranky = false
+            this.activities = 0
+        } else {
+            return 'Not while I\'m laying down!'
+        }
+    }
+}
+
+module.exports = Centaur
