5dfe0ff44b15a14fa0844c7a71affd704d5ce7a8
Christopher Baum <christopherabaum@gmail.com>
2022-12-27 15:31:37 -0600

    Complete all tests for golf-course.js and golfer.js

--- golf-course/golf-course.js
+++ golf-course/golf-course.js
@@ -1,6 +1,23 @@
 class GolfCourse {
+    constructor(courseName, courseDifficulty, dayOpenings, courseFeatures) {
+        this.name = courseName
+        this.difficulty = courseDifficulty
+        this.openings = dayOpenings
+        this.features = courseFeatures
+        this.currentlyPlaying = []
+    }
 
-
+    checkInGroup(golfers) {
+        if (this.openings > golfers.length) {
+            this.openings -= golfers.length
+            for (var i = 0; i < golfers.length; i++) {
+                this.currentlyPlaying.unshift(golfers[i].name)
+            }
+            return 'You\'re checked in. Have fun!'
+        } else {
+            return 'Sorry, we are currently booked! Please come back later.'
+        }
+    }
 }
 
 module.exports = GolfCourse;
--- golf-course/golfer.js
+++ golf-course/golfer.js
@@ -1,6 +1,46 @@
 class Golfer {
+    constructor(person) {
+        this.name = person.name
+        this.handicap = person.handicap
+        this.frustration = 0
+        this.confidence = 0
+    }
 
+    calculateAvg(par) {
+        return `I usually shoot a ${(this.handicap + par)} on average.`
+    }
 
+    playRound(course) {
+        if (course.difficulty === 'hard') {
+            this.frustration += 500
+        }
+        if (course.difficulty === 'moderate') {
+            this.frustration += 100
+        }
+    }
+
+    hitTheRange() {
+        this.confidence += 10
+    }
+
+    marvel(course) {
+        return `I love the ${course.features[0]} and ${course.features[1]} on this course!`
+    }
+
+    whatYaShoot(num) {
+        if (num < 0) {
+            this.frustration = 0
+            return 'I AM THE GREATEST GOLFER ALIVE!'
+        }
+        if (num > 0) {
+            this.frustration += 20
+            return 'Doh!'
+        }
+        if (num === 0) {
+            this.frustration -= 10
+            return 'Booyah!'
+        }
+    }
 }
 
 module.exports = Golfer;
